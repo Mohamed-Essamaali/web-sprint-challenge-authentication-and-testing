6648e56a01b9daf1f4450d01d452ca9aa3ea2502
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2021-01-22 21:51:17 -0500

    in progress restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,7 @@
-module.exports = (req, res, next) => {
-  next();
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  // next();
   /*
     IMPLEMENT
 
@@ -11,4 +13,24 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+ try{
+   const token = req.cookies.token
+
+   if(!token){
+     return res.status(401).json({message: "token required"})
+   }
+   
+   jwt.verify(token,"terces ti peek, efas ti peek",(err,decoded)=>{
+     if(err){
+      return res.status(401).json({
+        message: "token invalid",
+      })
+     }
+     req.token = decoded
+    next()
+   })
+
+ }
+ catch(err){next(err)}
+
 };
